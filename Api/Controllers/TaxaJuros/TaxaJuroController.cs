56089b0883ab100710c98cc9e4ff4d712450f674
cataneomatheus <cataneo_matheus@hotmail.com>
2020-08-01 19:22:32 -0300

    Retorno taxa através do dominio e construtor

---
+++ Api/Controllers/TaxaJuros/TaxaJuroController.cs
@@ -0,0 +1,16 @@
+﻿using Dominio;
+using Dominio.TaxaJuros;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers.TaxaJuros
+{
+    [Route("TaxaJuro")]
+    public class TaxaJuroController : ControllerBase
+    {
+        [HttpGet]
+        public double GetTaxa()
+        {
+            return new TaxaJuro().Taxa;
+        }
+    }
+}
---
+++ Dominio/TaxaJuros/TaxaJuro.cs
@@ -0,0 +1,11 @@
+﻿namespace Dominio.TaxaJuros
+{
+    public class TaxaJuro
+    {
+        public TaxaJuro()
+        {
+            Taxa = 0.01;
+        }
+        public double Taxa { get; set; }
+    }
+}
